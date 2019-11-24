231d29dd6522f5498d96d4f0756e30642422b3ae
Austin Asbury <acasbury25@aol.com>
2019-11-24 16:27:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if session[:user_id] != nil
+    if session.has_key?(:user_id)
       erb :account
     else
       erb :error
