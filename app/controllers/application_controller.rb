96888199bd9e732649557f2333a2fa28574fa96b
Austin Asbury <acasbury25@aol.com>
2019-11-24 16:09:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,10 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = Users.find_by(username: params[:username], password: params[:password])
-
-    session[:user_id] = @user.id
-    redirect '/account'
-
+    if @user
+     session[:user_id] = @user.id
+     redirect '/account'
+    end
     redirect '/'
   end
 
